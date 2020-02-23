ba94929a38212084ef67129dcf17cbba75b79113
Dimasik <dim56026257@yandex.ru>
2020-02-23 16:09:27 +0300

    feat: add algorithm 'towel sort'

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
-
-// You should implement your task here.
-
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix) {
+    let result = [];
+    if (!matrix) {
+        return result;
+    } else {
+        matrix.forEach((elem, index) =>
+            !(index % 2) ? result.push(...elem) : result.push(...elem.reverse())
+        );
+        return result;
+    }
+};
